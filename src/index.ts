1b7a62d3dfe542d1e4ee9ecd7c51b92fdb3cb69d
Fernando Via Canel <fernando.via@gmail.com>
2021-09-12 16:04:36 +0200

    Make options object truly optional

--- src/index.ts
+++ src/index.ts
@@ -1,121 +1,126 @@
-import chalk from 'chalk'
-import * as diff from 'diff'
-import fs from 'fs'
-import path from 'path'
+import chalk from "chalk";
+import * as diff from "diff";
+import fs from "fs";
+import path from "path";
 
-type Result = boolean | number | string | [] | {} | undefined | null
+type Result = boolean | number | string | [] | {} | undefined | null;
 
-type Test = [string, () => Result]
+type Test = [string, () => Result];
 
 function iterate(diffs: diff.Change[], result: string[]): string[] {
   if (diffs.length === 0) {
-    return result
+    return result;
   }
 
-  const [diff, ...rest] = diffs
-  const value = diff.value.length === 1 ? diff.value : diff.value.slice(0, -1)
+  const [diff, ...rest] = diffs;
+  const value = diff.value.length === 1 ? diff.value : diff.value.slice(0, -1);
 
   if (diff.removed === true) {
     return iterate(rest, [
       ...result,
-      ...value.split('\n').map((line) => chalk.red(`- ${line}`)),
-    ])
+      ...value.split("\n").map((line) => chalk.red(`- ${line}`)),
+    ]);
   }
 
   if (diff.added === true) {
     return iterate(rest, [
       ...result,
-      ...value.split('\n').map((line) => chalk.green(`+ ${line}`)),
-    ])
+      ...value.split("\n").map((line) => chalk.green(`+ ${line}`)),
+    ]);
   }
 
-  return iterate(rest, [...result, ...value.split('\n')])
+  return iterate(rest, [...result, ...value.split("\n")]);
 }
 
 const overwriteTest = (filePath: string, result: Result) => {
-  if (typeof result === 'string') {
-    fs.writeFileSync(filePath, result)
+  if (typeof result === "string") {
+    fs.writeFileSync(filePath, result);
   } else {
-    fs.writeFileSync(path.resolve(filePath), JSON.stringify(result, null, 2))
+    fs.writeFileSync(path.resolve(filePath), JSON.stringify(result, null, 2));
   }
-}
+};
 
-const runTest = (snapshotsDirPath: string, overwrite: boolean) => ([
-  name,
-  test,
-]: Test) => {
-  const filePath = path.resolve(snapshotsDirPath, name)
-  let result: Result
+const runTest =
+  (snapshotsDirPath: string, overwrite: boolean) =>
+  ([name, test]: Test) => {
+    const filePath = path.resolve(snapshotsDirPath, name);
+    let result: Result;
 
-  try {
-    result = test()
-  } catch (e) {
-    console.error(chalk.red(chalk.bold('//failed')))
-    console.error(chalk.red(name))
-    console.error()
-    console.error(e)
-    process.exit(1)
-  }
-
-  if (!overwrite) {
-    let currentValue: string | null = null
     try {
-      currentValue = fs.readFileSync(filePath, 'utf-8')
+      result = test();
+    } catch (e) {
+      console.error(chalk.red(chalk.bold("//failed")));
+      console.error(chalk.red(name));
+      console.error();
+      console.error(e);
+      process.exit(1);
+    }
 
-      const resultString =
-        typeof result === 'string' ? result : JSON.stringify(result, null, 2)
+    if (!overwrite) {
+      let currentValue: string | null = null;
+      try {
+        currentValue = fs.readFileSync(filePath, "utf-8");
 
-      if (currentValue !== resultString) {
-        const theDiff = diff.diffLines(currentValue, resultString)
+        const resultString =
+          typeof result === "string" ? result : JSON.stringify(result, null, 2);
 
-        console.error(chalk.yellow(chalk.bold('//diff')))
-        console.error(chalk.yellow(name))
-        console.log()
+        if (currentValue !== resultString) {
+          const theDiff = diff.diffLines(currentValue, resultString);
 
-        iterate(theDiff, []).forEach((line) => console.log(line))
+          console.error(chalk.yellow(chalk.bold("//diff")));
+          console.error(chalk.yellow(name));
+          console.log();
 
-        console.log()
-        process.exit(1)
-      }
-    } catch (e) {}
-  }
+          iterate(theDiff, []).forEach((line) => console.log(line));
 
-  overwriteTest(filePath, result)
-}
+          console.log();
+          process.exit(1);
+        }
+      } catch (e) {}
+    }
+
+    overwriteTest(filePath, result);
+  };
 
 export const run = (
   tests: Test[],
-  basePath = '',
-  prefix = '__snapshots__',
+  basePath = "",
+  prefix = "__snapshots__",
   overwrite = false,
-  match?: string,
+  match?: string
 ) => {
-  const snapshotsDirPath = path.resolve(basePath, '..', prefix)
+  const snapshotsDirPath = path.resolve(basePath, "..", prefix);
   try {
-    fs.mkdirSync(snapshotsDirPath)
+    fs.mkdirSync(snapshotsDirPath);
   } catch (e) {}
 
-  const runTestWithPath = runTest(snapshotsDirPath, overwrite)
+  const runTestWithPath = runTest(snapshotsDirPath, overwrite);
 
   tests
     .filter(([name]) =>
-      match !== undefined ? new RegExp(match).test(name) : true,
+      match !== undefined ? new RegExp(match).test(name) : true
     )
-    .forEach(runTestWithPath)
-}
+    .forEach(runTestWithPath);
+};
 
 export const runFiles = (
   filePaths: string[],
-  {
-    snapshotsFolderName,
-    shouldOverwrite,
-    shouldMatch,
-  }: { snapshotsFolderName?: string, shouldOverwrite?: boolean, shouldMatch?: string}
+  options?: {
+    snapshotsFolderName?: string;
+    shouldOverwrite?: boolean;
+    shouldMatch?: string;
+  }
 ) => {
   filePaths.forEach((filePath) => {
-    const resolvedFilePath = path.resolve(filePath)
-    const { tests }: { tests: Test[] } = require(resolvedFilePath)
+    const resolvedFilePath = path.resolve(filePath);
+    const { tests }: { tests: Test[] } = require(resolvedFilePath);
 
-    run(tests, resolvedFilePath, snapshotsFolderName ?? '__snapshots__', shouldOverwrite ?? false, shouldMatch)
-  })
-}
+    run(
+      tests,
+      resolvedFilePath,
+      options?.snapshotsFolderName ?? "__snapshots__",
+      options?.shouldOverwrite ?? false,
+      options?.shouldMatch
+    );
+  });
+};
